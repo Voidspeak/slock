a0edfb1ba36dbe6ec60e939b3fc6cf4278573497
Predrag Perisic <predrag3.14159@gmail.com>
2021-02-10 15:35:01 +0100

    set user and group

--- config.def.h
+++ config.def.h
@@ -1,6 +1,6 @@
 /* user and group to drop privileges to */
-static const char *user  = "nouser";
-static const char *group = "nogroup";
+static const char *user  = "predrag";
+static const char *group = "predrag";
 
 static const char *colorname[NUMCOLS] = {
 	[INIT] =   "black",     /* after initialization */
